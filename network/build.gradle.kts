b49651a792c1a2f8e892e04361020ec401799acc
justmaxiz <maxdonater12@gmail.com>
2026-03-31 12:41:58 +0400

    init commit

---
+++ network/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "ru.justmaxiz.saransk.network"
+    compileSdk = 34 // Или 35, смотря что в основном проекте
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
